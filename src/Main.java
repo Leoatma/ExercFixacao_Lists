0c19572dde3a82538d165fc2bcd2795cfa961c7d
lsAguiar <lsaguiar.code@gmail.com>
2023-07-05 10:49:57 -0300

    scanner closed

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,8 @@ public class Main {
 
         imprimirListaFuncionarios();
 
+        scanner.close();
+
     }
 
     public static void cadastrarFuncionários(int qtdeFuncionarios, Scanner scanner) {
